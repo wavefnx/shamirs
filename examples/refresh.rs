14b4a49db6592e239f167b76f2d5727b7b4179be
wavefnx <157986149+wavefnx@users.noreply.github.com>
2024-12-01 18:20:06 +0100

    repo(examples): improve readability on the refresh example

--- examples/refresh.rs
+++ examples/refresh.rs
@@ -1,6 +1,6 @@
 #[cfg(feature = "refresh")]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    use rand::{prelude::SliceRandom, RngCore};
+    use rand::{thread_rng, RngCore};
     use shamirs::{combine, refresh, split};
 
     /// The total number of shares the secret will be split into.
@@ -8,19 +8,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     /// The minimum number of shares needed to reconstruct the secret.
     const THRESHOLD: usize = 3;
 
-    // Initialize secret buffer with random bytes
-    let mut original_secret: [u8; 32] = [0; 32];
-    rand::thread_rng().fill_bytes(&mut original_secret);
-
-    println!("secret: {}\n", hex::encode(&original_secret));
-
     // ----------------------------------------
     // --- Stage I: Initial Share Generation --
     // ----------------------------------------
 
+    // Initialize secret buffer with random bytes
+    let mut secret = [0u8; 32];
+    thread_rng().fill_bytes(&mut secret);
+    println!("secret: {}\n", hex::encode(&secret));
+
     // Split the secret into shares
     println!("# initial shares:");
-    let mut initial_shares: Vec<Vec<u8>> = split(&original_secret, NUM_SHARES, THRESHOLD)?;
+    let shares = split(&secret, NUM_SHARES, THRESHOLD)?;
     // This will generate `NUM_SHARES` amount of shares, with their corresponding x-coordinates.
     //      y0   y1   y2   y3   y4   y5   y6   y7   y8   y9  y10  y11  y12  y13   x
     // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
@@ -28,20 +27,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // s2 = 46 | 50 | 1f | 7b | 38 | cc | 46 | eb | 90 | dc | a1 | 5e | 37 | 48 | 45
     // ...
 
-    // Randomly shuffle the shares for demonstrational purposes
-    initial_shares.shuffle(&mut rand::thread_rng());
     // Print all shares in hex encoding
-    for (i, share) in initial_shares.iter().enumerate() {
+    for (i, share) in shares.iter().enumerate() {
         println!("share_{}: {}", i + 1, hex::encode(share));
     }
 
     // Combine subset of shares to recover secret
-    let selected_shares: Vec<Vec<u8>> = initial_shares[..THRESHOLD].to_vec();
-    let recovered_secret: Vec<u8> = combine(&selected_shares)?;
-    // Verify that the recovery was successful
-    assert_eq!(original_secret, recovered_secret.as_slice());
+    let recovered = combine(&shares[..THRESHOLD])?;
+    assert_eq!(secret, recovered.as_slice());
 
-    println!("\nrecovered: {}", hex::encode(&recovered_secret));
+    println!("\nrecovered: {}", hex::encode(&recovered));
     println!("secret successfully reconstructed from shares.\n");
 
     // ----------------------------------------
@@ -50,26 +45,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Refresh the shares
     println!("# refreshed shares:");
-    let refreshed_shares = refresh(&initial_shares, THRESHOLD)?;
+    let refreshed = refresh(&shares, THRESHOLD)?;
 
     // Print all refreshed shares in hex encoding
-    for (i, share) in refreshed_shares.iter().enumerate() {
+    for (i, share) in refreshed.iter().enumerate() {
         println!("share_{}: {}", i + 1, hex::encode(share));
     }
 
     // Combine a subset of the refreshed shares to recover the secret
-    let selected_shares: Vec<Vec<u8>> = refreshed_shares[..THRESHOLD].to_vec();
-    let final_secret = combine(&selected_shares)?;
-
-    // Verify that the refreshed shares can reconstruct to the original secret
-    assert_eq!(original_secret, final_secret.as_slice());
+    let final_secret = combine(&refreshed[..THRESHOLD])?;
+    assert_eq!(secret, final_secret.as_slice());
 
     println!("\nrecovered: {}", hex::encode(&final_secret));
     println!("secret successfully reconstructed from refreshed shares.");
 
     Ok(())
 }
-
 
 // Fallback function if the refresh feature is not enabled
 #[cfg(not(feature = "refresh"))]
